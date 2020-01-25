127f301c7680b7767ac6f8087ae9ece5949dcc9a
Marcus Jensen <marcusjensen1515@gmail.com>
2020-01-25 11:48:36 -0800

    add basic posts controller. Yet to build views for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,29 +1,53 @@
 class PostsController < ApplicationController
 
   def show
-
+    @post = Post.find(params[:id])
+    render :show
   end
 
   def index
-
+    @posts = Post.all
+    render :index
   end
 
   def create
-
+    @post = Post.new(post_params)
+    if @post.save
+      # flash[:notice] = "Post was sucessfully added!" - haven't added flash messages yet, but will comment this out eventually
+      redirect_to :index
+    else
+      render :new
+    end
   end
 
   def new
+    @post = Post.new
+    render :new
+  end
 
-  end 
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to :posts_path
+    else
+      render :edit
+    end
+  end
 
   def edit
-
+    @post = Post.find(params[:id])
+    render :edit
   end
 
   def destroy
-
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to :index  
   end
 
-
+  private
+  def post_params
+    params.require(:post).permit(:title, :content_body, :date)
+  end
 
 end
