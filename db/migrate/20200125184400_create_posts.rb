30e144041969a624c3426eff417394d17ddfa58c
Marcus Jensen <marcusjensen1515@gmail.com>
2020-01-25 10:54:05 -0800

    sucessfully created posts database and test database. Sucessfully connected to rails

--- db/migrate/20200125184400_create_posts.rb
+++ db/migrate/20200125184400_create_posts.rb
@@ -1,6 +1,12 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
-    create_table :posts do |t|
+    create_table (:posts) do |t|
+      t.column(:title, :string)
+      t.column(:content_body, :string)
+      t.column(:date, :string)
+      #will eventually add photos to each blog post
+      #will perhaps eventually add a tag to each blog post
+      t.timestamps()
     end
   end
 end
