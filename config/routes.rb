6acc29884c4e8e93ac041effeb17fc1932093ced
Marcus Jensen <marcusjensen1515@gmail.com>
2020-01-25 11:12:54 -0800

    made the index post page the root path while developing posts feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #this will create full CRUD for posts 
+  #this will create full CRUD for posts
+
+  root to: 'posts#index'
+
   resources :posts
+
+  #making the index for posts the primary root while developing the posts portion of the site
+
+
+
 end
