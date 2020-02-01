de550b814682ac0100248d9e8a1b704a48c0b521
Marcus Jensen <marcusjensen1515@gmail.com>
2020-02-01 12:14:52 -0800

    rspec recognizing tests, initial test for .all method passing

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 describe Post do
   # Tests will go here.
+
+  describe('.all') do
+    it("returns an empty array when there are no posts") do
+      expect(Post.all).to(eq([]))
+    end
+  end
 end
