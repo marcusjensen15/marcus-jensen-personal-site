400c32b5310aa04caafcb044495b9ae7f65b4802
Marcus Jensen <marcusjensen1515@gmail.com>
2020-02-01 12:25:49 -0800

    save, delete and update methods all passing their tests

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -8,4 +8,33 @@ describe Post do
       expect(Post.all).to(eq([]))
     end
   end
+  describe('.save') do
+    it("Allows the user to create and save a new post") do
+      @post = Post.new({:title => "Giant Steps", :date => 2000, :content_body => "bluegrass"})
+      @post.save
+      expect(Post.all).to(eq([@post]))
+    end
+  end
+  describe('.delete') do
+    it("Allows the user remove a post from the content library") do
+      @post = Post.new({:title => "Giant Steps", :date => 2000, :content_body => "bluegrass"})
+      @post.save
+      @post.delete
+      expect(Post.all).to(eq([]))
+    end
+  end
+
+  describe('.update') do
+    it("Allows the user to update a previous post") do
+      @post = Post.new({:title => "Giant Steps", :date => 2000, :content_body => "bluegrass"})
+      @post.save
+      @post.update({:title => "Puppy Chow"})
+      expect(@post.title).to(eq("Puppy Chow"))
+    end
+  end
 end
+
+
+# t.column(:title, :string)
+# t.column(:content_body, :string)
+# t.column(:date, :string)
